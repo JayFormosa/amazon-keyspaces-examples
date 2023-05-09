ce278ab5ae66c9130333962d25e501c453d848b4
EC2 Default User <ec2-user@ip-172-31-20-10.eu-west-2.compute.internal>
2023-05-09 01:23:23 +0000

    modified:   README.md 	deleted:    __init__.py 	deleted:    keyspaces_example_function/__init__.py 	modified:   keyspaces_example_function/app.py 	modified:   template.yaml 	deleted:    tests/__init__.py 	deleted:    tests/integration/__init__.py 	deleted:    tests/integration/test_api_gateway.py 	deleted:    tests/requirements.txt 	deleted:    tests/unit/__init__.py 	deleted:    tests/unit/test_handler.py

--- python/datastax-v4/aws-lambda/lambda-example/lambda-keyspaces-example/keyspaces_example_function/app.py
+++ python/datastax-v4/aws-lambda/lambda-example/lambda-keyspaces-example/keyspaces_example_function/app.py
@@ -1,42 +1,45 @@
+from cassandra.cluster import Cluster, EXEC_PROFILE_DEFAULT, ConsistencyLevel
+from cassandra.policies import RoundRobinPolicy
+from ssl import SSLContext, PROTOCOL_TLSv1_2 , CERT_REQUIRED
+from cassandra.auth import PlainTextAuthProvider
+import boto3
+from cassandra_sigv4.auth import SigV4AuthProvider
+
 import json
+import os
 
-# import requests
+import base64
+from botocore.exceptions import ClientError
 
+aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
+aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
+aws_session_token = os.environ['AWS_SESSION_TOKEN']
+
+ssl_context = SSLContext(PROTOCOL_TLSv1_2)
+ssl_context.load_verify_locations('/opt/certificate/sf-class2-root.crt')
+ssl_context.verify_mode = CERT_REQUIRED
+
+# region name wil have to become an envronment variable in the lambda function configuration
+auth_provider = SigV4AuthProvider(aws_access_key_id=aws_access_key_id,
+                                  aws_secret_access_key=aws_secret_access_key,
+                                  aws_session_token=aws_session_token,
+                                  region_name='eu-west-2')
+    
+# the cluster will have to become an environemtn variable in the lambda function configuration
+cluster = Cluster(['cassandra.eu-west-2.amazonaws.com'], ssl_context=ssl_context, auth_provider=auth_provider,
+                  port=9142, protocol_version=4, load_balancing_policy=RoundRobinPolicy())
+                  
+session = cluster.connect()
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
+    record = json.loads(event['Records'][0]['body'])
+    print(record)
+    
+    stmt = session.prepare("INSERT INTO securities.dividend_history (symbol, company) VALUES (?, ?)")
+    stmt.consistency_level = consistency_level=ConsistencyLevel.LOCAL_QUORUM
+    session.execute(stmt, (record['symbol'],record['company']))
+    
     return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
     }
